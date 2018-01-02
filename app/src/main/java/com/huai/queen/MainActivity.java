fe55a7618daaffd56fe314183c034803f508495a
huaijung Huang <je3rjoy@gmail.com>
2018-01-02 12:10:25 +0800

    Firebase

--- app/src/main/java/com/huai/queen/MainActivity.java
+++ app/src/main/java/com/huai/queen/MainActivity.java
@@ -5,14 +5,77 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private TextView tvQuestion;
+    private TextView tvAnswer;
+    private TextView tvAnswer1;
+    private TextView tvAnswer2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvQuestion = (TextView) findViewById(R.id.question);
+        tvAnswer = (TextView) findViewById(R.id.answer);
+        tvAnswer1 = (TextView) findViewById(R.id.answer);
+        tvAnswer2 = (TextView) findViewById(R.id.answer2);
+
+        //get data from Firebase
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference refQuestion = firebaseDatabase.getReference("question");
+        refQuestion.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String question = (String) dataSnapshot.getValue();
+                tvQuestion.setText(question);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+        DatabaseReference refAnswers = firebaseDatabase.getReference("answers");
+        refAnswers.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String key) {
+                Log.d(TAG, "onChildAdded: "+key+"/"+dataSnapshot.getValue());
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
